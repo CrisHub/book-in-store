0a200b5e1990cbb4d28999b8a00302ad38b51176
CrisHub <ccristian.moldovan@yahoo.com>
2016-09-01 22:29:46 +0300

    ..

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ exports.renderApp = function(req, res){
       });
       tagsArray = _.uniq(tagsArray);
       tagsArray = tagsArray.join(', ');
-      Shopify.post('/admin/products/7497850881.json', {
+      Shopify.put('/admin/products/7497850881.json', {
           "product": {
             "id": 7497850881,
             "tags": tagsArray
