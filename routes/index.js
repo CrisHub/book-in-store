40973cf122fb333f04f76a7be681fbbad913e67c
CrisHub <ccristian.moldovan@yahoo.com>
2016-09-14 22:27:25 +0300

    ..

--- routes/index.js
+++ routes/index.js
@@ -113,7 +113,7 @@ exports.renderApp = function(req, res){
 
     var getProducts = function(page, limit) {
       //274091393 is hardcoded
-      Shopify.get('/admin/products.json?page=3&limit=250', function(err, data, headers){
+      Shopify.get('/admin/products.json?page=4&limit=250', function(err, data, headers){
           allProd = data;
           setTags(allProd);
       });
